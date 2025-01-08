91aa4dc8a362bf857e4f65846506f359f5729f4f
MRXclay666777 <mukhammadsaid034@gmail.com>
2025-01-08 14:40:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const userName = prompt("Enter your name ")
+const userLogin = +prompt("Iltimos login kiriting ")
+const userPassword = +prompt("Iltmos parol kiriting ")
+
+
+const mars = {
+ism : userName,
+login : userLogin,
+password : userPassword,
+}
+
+
+console.log(mars);
